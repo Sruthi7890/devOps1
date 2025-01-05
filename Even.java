56ec61b721d9aa568cc25e8e7237a44cfe390794
sruthi7890 <kethavarapuharshitha@gmail.com>
2025-01-05 20:35:21 +0530

    commiting the file

---
+++ Even.java
@@ -0,0 +1,18 @@
+import java.uti.*;
+
+public class Even
+{
+    public static voidmain(String args[])
+    {
+        Scanner sc = new Scanner(Syste.in);
+        System.out.println("Enter the valu of n:");
+        int n = sc.nextInt();
+         for(i = 0; i < n; i++)
+         {
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
+         }
+    }
+    
+}
